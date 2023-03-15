5ce48c0eab4a465f20c3cc665988306c86708c14
Yuriy Cherkas <Yuriy.Cherkas@dataart.com>
2023-03-15 15:37:20 +0200

    Laboratory work 1 - Implementation

---
+++ lab1/Lab1/Concrete/NavigatorIterator.cs
@@ -0,0 +1,27 @@
+﻿using Lab1.Interfaces;
+using System;
+
+namespace Lab1.Concrete
+{
+    public class NavigatorIterator : IIterator<TouristPlace>
+    {
+        private readonly IIterableCollection<TouristPlace> _collection;
+        private int _index;
+
+        public NavigatorIterator(IIterableCollection<TouristPlace> collection)
+        {
+            _collection = collection;
+        }
+
+        public bool HasNext()
+        {
+            return _index < _collection.Count;
+        }
+
+        public TouristPlace Next()
+        {
+            //TODO: change to real implementation
+            return _collection[_index++];
+        }
+    }
+}
