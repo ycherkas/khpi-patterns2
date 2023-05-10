96b904e8e16f79bbdc1bfe0d21afb79216b88c0f
Yuriy Cherkas <Yuriy.Cherkas@dataart.com>
2023-05-10 10:06:19 +0300

    Builder implementation

---
+++ lab8/Inferfaces/IBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Lab8.Concrete;
+
+namespace Lab8.Inferfaces
+{
+    public interface IBuilder
+    {
+        void Reset();
+
+        void BuildSelect(string tableName);
+
+        void BuildWhere(List<Constraint> constraints);
+
+        void BuildLimit(int limit);
+
+        string GetSql();
+    }
+}
---
+++ lab8/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Lab8.Concrete;
+
+var mySqlBuilder = new MySqlBuilder();
+var postgreSqlBuilder = new PostgreSqlBuilder();
+
+var constraints = new List<Constraint>()
+{
+    new Constraint { ColumnName="FirstName", Operator="=", Value="Ivan" },
+    new Constraint { ColumnName="Age", Operator=">", Value=12, IsNumerical = true }
+};
+
+var director = new Director();
+
+director.Builder = mySqlBuilder;
+
+var query = director.BuildQuery("students");
+Console.WriteLine($"MySql: select:\r\n{query}");
+
+query = director.BuildQuery(tableName: "students", limit: 10);
+Console.WriteLine($"MySql: select + limit:\r\n{query}");
+
+query = director.BuildQuery(tableName: "students", constraints: constraints, limit: 10);
+Console.WriteLine($"MySql: select + where + limit:\r\n{query}");
+
+
+director.Builder = postgreSqlBuilder;
+query = director.BuildQuery(tableName: "students", constraints: constraints, limit: 10);
+Console.WriteLine($"PostgreSql: select + where + limit:\r\n{query}");
